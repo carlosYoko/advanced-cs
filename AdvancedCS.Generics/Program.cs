7ccb94f011df56c09f5b9df986dccf829a78079e
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-28 18:35:01 +0100

    generics in interfaces

--- AdvancedCS.Generics/Program.cs
+++ AdvancedCS.Generics/Program.cs
@@ -7,3 +7,18 @@ Console.WriteLine(box.GetContent());
 
 var box2 = new Box<bool>(false);
 Console.WriteLine(box2.GetContent());
+
+BikeRepository bikeRepository = new BikeRepository();
+bikeRepository.Add("Honda");
+bikeRepository.Add("Yamaha");
+
+Show(bikeRepository);
+
+void Show<T>(IRepository<T> repository)
+{
+    foreach (var item in repository.GetAll())
+    {
+        if (item == null) continue;
+        Console.WriteLine(item);
+    }
+}
