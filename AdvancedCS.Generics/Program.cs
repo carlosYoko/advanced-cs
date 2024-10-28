3c27c91395c8f8190ed4bcbbb3d2952483ac36f9
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-28 18:43:04 +0100

    generics in delegates

--- AdvancedCS.Generics/Program.cs
+++ AdvancedCS.Generics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdvancedCS.Generics;
 
+// Generics in classes
 var box = new Box<int>(7);
 Console.WriteLine(box.Get());
 box.Set(10);
@@ -8,6 +9,8 @@ Console.WriteLine(box.GetContent());
 var box2 = new Box<bool>(false);
 Console.WriteLine(box2.GetContent());
 
+
+// Generics in methods
 BikeRepository bikeRepository = new BikeRepository();
 bikeRepository.Add("Honda");
 bikeRepository.Add("Yamaha");
@@ -22,3 +25,16 @@ void Show<T>(IRepository<T> repository)
         Console.WriteLine(item);
     }
 }
+
+
+// Generics in delegates
+Operation<string> concat = (a, b) => a + " " + b;
+Console.WriteLine(concat("Hello", "World"));
+
+Operation<int> add = (a, b) => a + b;
+Console.WriteLine(add(5, 2));
+
+Operation<double> addDouble = (a, b) => a + b;
+Console.WriteLine(addDouble(5.3, 2.4));
+
+delegate T Operation<T>(T a, T b);
