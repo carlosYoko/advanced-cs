f2e5726f32f24598fd610a14911f205e33822567
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 13:11:49 +0100

    extension methods with params

--- AdvancedCS.ExtensionMethods/Program.cs
+++ AdvancedCS.ExtensionMethods/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdvancedCS.ExtensionMethods;
 
+// Basic extension method
 int number = 10;
 int number2 = number.X2();
 int number3 = number2.X2();
@@ -7,8 +8,15 @@ Console.WriteLine(number);
 Console.WriteLine(number2);
 Console.WriteLine(number3);
 
+// Extension methods in custom types
 var sale = new Sale(345.21m);
 Console.WriteLine(sale.GetInfo());
+
+// Extension methods with params
+var numberX3 = 4;
+var resultNumberX3 = numberX3.Mul(3);
+Console.WriteLine(resultNumberX3);
+
 
 
 // Basic extension method
@@ -20,12 +28,20 @@ public static class IntOperations
     }
 }
 
-
 // Extension methods in custom types
 public static class SaleExtensions
 {
     public static string GetInfo(this Sale sale)
     {
         return $"El total es: {sale.Amount}";
+    }
+}
+
+// Extension methods with params
+public static class IntOperationsWithParams
+{
+    public static int Mul(this int n, int multiplier)
+    {
+        return n * multiplier;
     }
 }
