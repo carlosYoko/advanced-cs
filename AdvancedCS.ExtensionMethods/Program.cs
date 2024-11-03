731767dc6effca193d7d0970732113928707ed6d
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 12:56:27 +0100

    create extension methods project

---
+++ AdvancedCS.ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 10;
+int number2 = number.X2();
+int number3 = number2.X2();
+
+Console.WriteLine(number);
+Console.WriteLine(number2);
+Console.WriteLine(number3);
+
+public static class IntOperations
+{
+    public static int X2(this int n)
+    {
+        return n * 2;
+    }
+}
