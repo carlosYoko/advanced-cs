ae6d98e50b1de2965a04e72459892f0ff486aea9
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 16:38:49 +0100

    Inmutability with props readonly

--- AdvancedCS.Inmutability/Program.cs
+++ AdvancedCS.Inmutability/Program.cs
@@ -1,6 +1,13 @@
-﻿var calculator = new Calculator(15);
+﻿// Inmutability 
+var calculator = new Calculator(15);
 Console.WriteLine(Calculator.PI); ;
 
+// Inmutability with props readonly
+var brand = new Brand("Honda");
+var brandName = brand.Name;
+
+
+// Inmutability
 public class Calculator
 {
     public const double PI = 3.1416;
@@ -11,3 +18,15 @@ public class Calculator
         Value = value;
     }
 }
+
+// Inmutability with props readonly
+public class Brand
+{
+    public string Name { get; }
+    public Brand(string name)
+    {
+        Name = name;
+    }
+}
+
+// Inmutable list
