9640d7432deb1b584670c33bc2228b71226b3e97
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 13:05:23 +0100

    extension methods in custom types

--- AdvancedCS.ExtensionMethods/Program.cs
+++ AdvancedCS.ExtensionMethods/Program.cs
@@ -1,15 +1,31 @@
-﻿int number = 10;
+﻿using AdvancedCS.ExtensionMethods;
+
+int number = 10;
 int number2 = number.X2();
 int number3 = number2.X2();
-
 Console.WriteLine(number);
 Console.WriteLine(number2);
 Console.WriteLine(number3);
 
+var sale = new Sale(345.21m);
+Console.WriteLine(sale.GetInfo());
+
+
+// Basic extension method
 public static class IntOperations
 {
     public static int X2(this int n)
     {
         return n * 2;
+    }
+}
+
+
+// Extension methods in custom types
+public static class SaleExtensions
+{
+    public static string GetInfo(this Sale sale)
+    {
+        return $"El total es: {sale.Amount}";
     }
 }
---
+++ AdvancedCS.ExtensionMethods/Sale.cs
@@ -0,0 +1,12 @@
+﻿namespace AdvancedCS.ExtensionMethods
+{
+    public class Sale
+    {
+        public decimal Amount { get; set; }
+
+        public Sale(decimal amount)
+        {
+            Amount = amount;
+        }
+    }
+}
