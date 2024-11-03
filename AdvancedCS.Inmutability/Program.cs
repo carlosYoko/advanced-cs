2ea0fdfda80c7f0a5c98c463c3e2b54953b12266
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 16:26:16 +0100

    inmutability

---
+++ AdvancedCS.Inmutability/Program.cs
@@ -0,0 +1,13 @@
+﻿var calculator = new Calculator(15);
+Console.WriteLine(Calculator.PI); ;
+
+public class Calculator
+{
+    public const double PI = 3.1416;
+    public readonly int Value;
+
+    public Calculator(int value)
+    {
+        Value = value;
+    }
+}
