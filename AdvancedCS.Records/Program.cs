0cc59d22570f2e487a5f4de7cec18718cd1ae5bf
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-03 18:21:51 +0100

    records

---
+++ AdvancedCS.Records/Program.cs
@@ -0,0 +1,16 @@
+﻿// ########
+// # Uses #
+// ########
+
+// Basic record
+var beer = new Bike("CBF", 600);
+Console.WriteLine(beer.Brand);
+Console.WriteLine(beer.Engine);
+
+
+// ##################
+// # Implementation #
+// ##################
+
+// Basic record
+record Bike(string Brand, double Engine);
