5b8026f418dbeef45f8a9cfff7243602e8c4ca2e
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-07 22:29:18 +0100

    custom map method

---
+++ AdvancedCS.FunctionalProgramming/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace AdvancedCS.FunctionalProgramming
+{
+    public static class ListExtensions
+    {
+        public static List<TOutput> MyMap<TInput, TOutput>(this List<TInput> lst, Func<TInput, TOutput> fn)
+        {
+            List<TOutput> lstResult = new List<TOutput>();
+
+            foreach (TInput item in lst)
+            {
+                TOutput output = fn(item);
+                lstResult.Add(output);
+            }
+
+            return lstResult;
+        }
+    }
+}
--- AdvancedCS.FunctionalProgramming/Program.cs
+++ AdvancedCS.FunctionalProgramming/Program.cs
@@ -9,3 +9,26 @@ someSum(2, 4);
 
 var delay = Closure.DelayExecuter(3000, () => Console.WriteLine("Funcion ejecutandose"));
 delay();
+
+// Map
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var evenNumbers = numbers.MyMap<int, string>(e =>
+{
+    if (e % 2 == 0)
+    {
+        return "Par";
+    }
+    return "Impar";
+});
+
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(number);
+}
+
+var stringNumbers = numbers.MyMap<int, string>(e => $"El numero es {e}");
+foreach (var i in stringNumbers)
+{
+    Console.WriteLine(i);
+}
