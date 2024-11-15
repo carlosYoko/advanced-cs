6030d468f45e61e67fa616954f662901ef53cd3d
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-15 17:29:01 +0100

    Task

--- AdvancedCS.Concurrent/Program.cs
+++ AdvancedCS.Concurrent/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Task
+Console.WriteLine("Inicio programa");
+
+var task = Task.Run(async () =>
+{
+    Console.WriteLine("Inicio task");
+
+    await Task.Delay(1000);
+
+    Console.WriteLine("Fin task");
+});
+
+Console.WriteLine("Algo extra");
+
+await task;
+
+Console.WriteLine("Algo extra despues");
+
+
+Console.WriteLine("Fin programa");
