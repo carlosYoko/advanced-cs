edf34fe790e9757d06467a94efbafb1b354e03c4
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-17 11:26:58 +0100

    ContinueWith

--- AdvancedCS.Concurrent/Program.cs
+++ AdvancedCS.Concurrent/Program.cs
@@ -35,4 +35,31 @@ Console.WriteLine("Fin del programa");
 // TResult
 var taskResult = Methods.AddAsync(3, 4);
 var number = await taskResult;
-Console.WriteLine(taskResult);
+Console.WriteLine(number);
+
+
+// ContinueWith
+var tasks = Task.Run(() =>
+{
+    Console.WriteLine("Tarea incial secuencial INICIA");
+
+    var resultTask = Methods.AddAsync(4, 4);
+
+    Console.WriteLine("Tarea incial secuencial FINALIZA");
+
+    return resultTask.Result;
+
+}).ContinueWith((resultTask) =>
+{
+    Console.WriteLine("Segunda tarea secuencial INICIA");
+    var result = resultTask.Result;
+    Console.WriteLine($"El resultado es {result}");
+
+    Task.Delay(2000).Wait();
+
+    Console.WriteLine("Segunda tarea secuencial FINALIZA");
+});
+
+await tasks;
+
+Console.WriteLine("Fin de las tareas secuenciales");
